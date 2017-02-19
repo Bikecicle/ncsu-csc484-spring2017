7ec33f38c5f848489434526b9a99760de324d15e
winlap <winlap@liblend-39235>
2017-02-19 14:16:02 -0500

    starated

---
+++ csc484-hw1/src/kinematic_motion/View.java
@@ -0,0 +1,30 @@
+package kinematic_motion;
+
+import processing.core.PApplet;
+
+public class View extends PApplet{
+	
+	public static final int viewWidth = 800;
+	public static final int viewHeight = 600;
+
+    public static void main(String[] args) {
+        PApplet.main("kinematic_motion.View");
+    }
+
+    public void settings(){
+        size(viewWidth,viewHeight);
+    }
+
+    public void setup(){
+        fill(120,50,240);
+    }
+
+    public void draw(){
+        
+    }
+    
+    private void renderChar( Character c ) {
+    	
+    }
+	
+}
