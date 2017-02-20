e6b07f314f71811ebc96a28dfda7363618437a58
winlap <winlap@liblend-39227>
2017-02-19 22:06:12 -0500

    herpdider[

---
+++ csc484-hw1/src/algorithms/Seek.java
@@ -0,0 +1,19 @@
+package algorithms;
+
+import general.Static;
+import general.SteeringOutput;
+
+public class Seek {
+
+	private Static character;
+	private Static target;
+	
+	public Seek() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public SteeringOutput getSteering() {
+		SteeringOutput steering = new SteeringOutput();
+		
+	}
+}
---
+++ csc484-hw1/src/general/Static.java
@@ -0,0 +1,7 @@
+package general;
+
+public class Static {
+
+	public Vector position;
+	public double orientation;
+}
